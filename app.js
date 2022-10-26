5104baf13abb73aecd6893d9dde5a207d86bfe93
harithareddy630 <haritha.rdy630@gmail.com>
2022-10-26 11:34:07 -0500

    updated app.js file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var mydataRouter = require('./routes/mydata');
 var app = express();
 
 // view engine setup
@@ -21,10 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/mydata', mydataRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404)); 
 });
 
 // error handler
